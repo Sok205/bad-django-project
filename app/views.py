4e28ea2cd6ec12d41313729a8023d9de753b897e
rkorzen <korzeniewski@gmail.com>
2025-02-16 21:40:12 +0100

    changes

--- app/views.py
+++ app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def book_list(request):
-    books = Book.objects.all()
+    books = Book.all()
     return render(request, "app/book_list.html", {"books": books})
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    return render(request, "app/book_detail.html", {"book": book})
+    return render(request, "books/book_detail.html", {"book": book})
